291169268d610ae8c764c4639f061edc01194b7b
Sriram Kumar <75881342+BatteringRam-dev@users.noreply.github.com>
2020-12-12 16:38:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+series_name = "Peaky Blinders"
+character_name = "Thomas Shelby"
+print("A Netflix original, " + series_name + " is a real criminal action story taking place in Birmingham,England during the 1920s.")
+print("It shows the growth of the protagonist " + character_name.upper() + ".")
+print("Now, in this story," + character_name + " has 4 brothers and a sister.")
+
+character_name2 = "Arthur Shelby"
+print("One of his brother's name is " + character_name2.upper() + ".")
+print("His brother, " + character_name2.upper() + " is a brute and very rage guy but doesn't have that much brains to think." )
+
+character_name3 = "Ada Shelby"
+print("His sister, " + character_name3.upper() + " changes her name to " + character_name3.replace("Shelby" , "Thorne") + " after marrying a communist Freddie Throne. ")
+
