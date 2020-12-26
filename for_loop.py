39ee56f5fc4d9d5477fb58af20f08c7aaa623176
Sriram <stadia768@gmail.com>
2020-12-27 00:07:28 +0530

    Detailed For Loop

--- for_loop.py
+++
@@ -1,7 +0,0 @@
-# This file is used to understand how for loop works
-
-
-x = 5;
-
-for number in range(x):
-    print(number+1)
--- forloop2.py
+++ forloop2.py
@@ -3,11 +3,17 @@ friends = ["Kevin", "Arthur", "Thomas"]
 for word in friends:
     print(word)
 
+# -------------------------------------------
+
 for index in range(len(friends)):
     print(index)
+
+# -------------------------------------------
 
 for index in range(10):
     if index == 0:
         print("First Iteration")
     else:
         print("Not an iteration")
+
+# -------------------------------------------
