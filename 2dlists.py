6d37584639b606a1e49fc5f21c03b86e780bf074
Sriram <stadia768@gmail.com>
2020-12-27 00:09:05 +0530

    2D lists and nested loops

---
+++ 2dlists.py
@@ -0,0 +1,14 @@
+
+# 2D Lists are 2 dimensional lists where a list is created inside a list.
+# 2D lists are very useful in creating a grid like structure.
+
+
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+for row in number_grid:
+    print(row)
